f9f06afefe437f7c5e41c4e329ad6c1d04c7f3d5
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-05 13:51:41 +0800

    fix src\errors\http.error.ts: Make sendResponse method asynchronous

--- src/errors/http.error.ts
+++ src/errors/http.error.ts
@@ -4,5 +4,5 @@ export abstract class HttpError extends Error {
     constructor(message: string) {
         super(message);
     }
-    abstract sendResponse(): BedResponse;
+    abstract sendResponse(): Promise<BedResponse>;
 }
