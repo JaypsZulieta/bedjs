e0df34fb86ced971af7e275da7687cf1e3c312b0
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-05 16:25:59 +0800

    feat src\json-object-parser\index.ts: Add string releated methods to  JsonObjectParser class

---
+++ src/json-object-parser/index.ts
@@ -0,0 +1,61 @@
+import { BadRequestError } from "../errors/bad-request.error";
+import { z } from "zod";
+
+export class JsonObjectParser {
+    constructor(private jsonObject: any, private parentObjectKey?: string) {}
+
+    private getFullkeyName(key: string): string {
+        if (!this.parentObjectKey) return key;
+        const fullKeyName = `${this.parentObjectKey}.${key}`;
+        return fullKeyName;
+    }
+
+    async getString(key: string): Promise<string> {
+        const value = this.jsonObject[key] as string;
+        const fullKeyName = this.getFullkeyName(key);
+        if (!value)
+            throw new BadRequestError(`${fullKeyName} must not be empty`);
+        const parseFailure = !(await z.string().safeParseAsync(value)).success;
+        if (parseFailure)
+            throw new BadRequestError(`${fullKeyName} must be a string`);
+        return value;
+    }
+
+    async getStringOptional(key: string): Promise<string | undefined> {
+        const value = this.jsonObject[key] as string;
+        if (!value) return undefined;
+        return this.getString(key);
+    }
+
+    async getStringArray(key?: string): Promise<string[]> {
+        let value: string[];
+        if (!key) {
+            value = this.jsonObject as string[];
+            if (value == undefined)
+                throw new BadRequestError("body must not be empty");
+            const parseFailure = !(
+                await z.array(z.string()).safeParseAsync(value)
+            ).success;
+            if (parseFailure)
+                throw new BadRequestError("body must be an array of strings");
+            return value;
+        }
+        const fullKeyName = this.getFullkeyName(key as string);
+        value = this.jsonObject[key] as string[];
+        if (value == undefined)
+            throw new BadRequestError(`${fullKeyName} must not be empty`);
+        const parseFailure = !(await z.array(z.string()).safeParseAsync(value))
+            .success;
+        if (parseFailure)
+            throw new BadRequestError(
+                `${fullKeyName} must be an array of strings`
+            );
+        return value;
+    }
+
+    async getStringArrayOptional(key: string): Promise<string[] | undefined> {
+        const value = this.jsonObject[key] as string[];
+        if (value == undefined) return undefined;
+        return this.getStringArray(key);
+    }
+}
