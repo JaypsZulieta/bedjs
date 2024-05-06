dbc711436b37746e692eb294808da116b34cb470
= <=>
2024-05-06 15:03:06 +0800

    feat src\bed-response\index.ts: Add JSDOC comments to BedResponse class

--- src/bed-response/index.ts
+++ src/bed-response/index.ts
@@ -1,11 +1,28 @@
 import { StatusCode } from "hono/utils/http-status";
 
+/**
+ * Represents a response handler class.
+ */
 export class BedResponse {
+    /**
+     * Creates an instance of BedResponse.
+     * @param {StatusCode} statusCode - The status code of the response.
+     * @param {unknown} body - The body of the response.
+     */
     constructor(private statusCode: StatusCode, private body: unknown) {}
 
+    /**
+     * Retrieves the status code of the response.
+     * @returns {StatusCode} The status code of the response.
+     */
     getStatusCode(): StatusCode {
         return this.statusCode;
     }
+
+    /**
+     * Retrieves the body of the response.
+     * @returns {unknown} The body of the response.
+     */
     getBody(): unknown {
         return this.body;
     }
