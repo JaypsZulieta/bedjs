87d4e8e6e50364c09e5968a053280d2b7cb599c4
= <=>
2024-05-06 15:11:56 +0800

    feat src\errors\forbidden.error.ts: Add JSDOC comments for ForbiddenError class

--- src/errors/forbidden.error.ts
+++ src/errors/forbidden.error.ts
@@ -2,11 +2,23 @@ import { BedResponse } from "../bed-response";
 import { BedResponseBuilder } from "../bed-response-builder";
 import { HttpError } from "./http.error";
 
+/**
+ * Represents an error indicating a forbidden request (HTTP 403).
+ * @extends HttpError
+ */
 export class ForbiddenError extends HttpError {
+    /**
+     * Creates an instance of ForbiddenError.
+     * @param {string} message - The error message.
+     */
     constructor(message: string) {
         super(message);
     }
 
+    /**
+     * Sends an HTTP response for the forbidden error.
+     * @returns {Promise<BedResponse>} A promise resolving to the HTTP response.
+     */
     async sendResponse(): Promise<BedResponse> {
         const message = this.message;
         return new BedResponseBuilder()
