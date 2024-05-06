027bb3c6e30d1257f811c6a27c8a43d386528da6
= <=>
2024-05-06 15:08:03 +0800

    feat src\errors\http.error.ts: Add JSDOC comments for HttpError class

--- src/errors/http.error.ts
+++ src/errors/http.error.ts
@@ -1,8 +1,22 @@
 import { BedResponse } from "../bed-response";
 
+/**
+ * Represents an abstract HTTP error class.
+ * @extends Error
+ */
 export abstract class HttpError extends Error {
+    /**
+     * Creates an instance of HttpError.
+     * @param {string} message - The error message.
+     */
     constructor(message: string) {
         super(message);
     }
+
+    /**
+     * Abstract method to send an HTTP response.
+     * @abstract
+     * @returns {Promise<BedResponse>} A promise resolving to the HTTP response.
+     */
     abstract sendResponse(): Promise<BedResponse>;
 }
