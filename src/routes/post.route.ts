b88c32ad4dc7586eb409e584516a437ea2941d86
= <=>
2024-05-06 15:29:15 +0800

    feat src\routes\post.route.ts: Add POSTRoute interface and deleted unused index file

---
+++ src/routes/post.route.ts
@@ -0,0 +1,17 @@
+import { BedRequest } from "../bed-request";
+import { BedResponse } from "../bed-response";
+import { HttpRoute } from "./http.route";
+
+/**
+ * Represents a POST route extending the HTTP route interface.
+ * @interface
+ * @extends HttpRoute
+ */
+export interface POSTRoute extends HttpRoute {
+    /**
+     * Handles POST requests and returns a response.
+     * @param {BedRequest} request - The request object.
+     * @returns {Promise<BedResponse>} A promise that resolves to the response object.
+     */
+    POST(reqeust: BedRequest): Promise<BedResponse>;
+}
