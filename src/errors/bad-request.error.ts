6329f83af145a176b6356e87306123f6cfed73f7
= <=>
2024-05-06 15:10:11 +0800

    feat src\errors\bad-request.error.ts: Add JSDOC comments for BadRequestError class

--- src/errors/bad-request.error.ts
+++ src/errors/bad-request.error.ts
@@ -2,11 +2,23 @@ import { BedResponse } from "../bed-response";
 import { BedResponseBuilder } from "../bed-response-builder";
 import { HttpError } from "./http.error";
 
+/**
+ * Represents an error indicating a bad request (HTTP 400).
+ * @extends HttpError
+ */
 export class BadRequestError extends HttpError {
+    /**
+     * Creates an instance of BadRequestError.
+     * @param {string} message - The error message.
+     */
     constructor(message: string) {
         super(message);
     }
 
+    /**
+     * Sends an HTTP response for the bad request error.
+     * @returns {Promise<BedResponse>} A promise resolving to the HTTP response.
+     */
     async sendResponse(): Promise<BedResponse> {
         const message = this.message;
         return new BedResponseBuilder()
